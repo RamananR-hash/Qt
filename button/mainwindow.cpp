bfde345af51ade6bf850e490a3dbac02259b5590
Arunchellasp <62378334+Arunchellasp@users.noreply.github.com>
2022-02-25 19:32:40 +0530

    Add files via upload

--- button/mainwindow.cpp
+++ button/mainwindow.cpp
@@ -6,15 +6,6 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QPixmap pix;
-
-
-        pix.load(":/rsc/icons png/pin.png");
-         QPixmap pix1 =pix.scaled(200,200);
-
-           ui->pushButton->setIcon(pix1);
-           ui->pushButton->setIconSize(pix1.rect().size());
-           ui->pushButton->setFixedSize(200,200);
 }
 
 MainWindow::~MainWindow()
